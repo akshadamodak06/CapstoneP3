2347ac479f38ccf1ec072ac4a754a2858963fde5
HaritaRathod <harita34rathod@gmail.com>
2024-04-02 20:03:06 +0530

    Commiting InsertNew function

--- EntryQueueManager_class.cpp
+++ EntryQueueManager_class.cpp
@@ -1,1 +1,23 @@
-
+void InsertNew( int arr_of_size[] , int gates )
+    {
+        cout << "Enter the no. of people entering: " ;
+        int people ;
+        cin >> people ;
+        int index_of_min = 0 , i , min = arr_of_size[0] ;
+        // min is the no. of people in queue containing the least amount of people
+        //index_of_min is the queue no. of said queue
+        while( people != 0 )
+        {
+            min =  arr_of_size[0] ;
+            for(i = 0 ; i < gates ; i++ )
+            {
+                if( arr_of_size[i] < min )
+                {
+                    min = arr_of_size[i] ;
+                    index_of_min = i ;
+                }
+            }
+            arr_of_size[index_of_min]++ ;
+            people-- ;
+        }
+    }
