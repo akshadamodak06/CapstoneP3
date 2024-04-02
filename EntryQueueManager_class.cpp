00b3c12694ca3d455d802b01e9bc635ee101195a
MCG-24115 <maanavgurubaxani2411@gmail.com>
2024-04-02 20:11:06 +0530

    Class

--- EntryQueueManager_class.cpp
+++ EntryQueueManager_class.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+class EntryQueueManager
+{
+private:
+    int gates;
+    int population;
+    int *arr_of_size;
+
+public:
+    EntryQueueManager(int p, int g)
+    {
+        gates = g;
+        population = p;
+        arr_of_size = new int[gates];
+        for (int i = 0; i < gates; ++i)
+            arr_of_size[i] = 0;
+    }
+
+    void display()
+    {
+        for (int counter = 0; counter < gates; counter++)
+        {
+            cout << "The number of members in Queue " << counter + 1 << " is : " << arr_of_size[counter] << endl;
+        }
+    }
+};
