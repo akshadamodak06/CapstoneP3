470d513d23d6cbb221200c638fe7a24ec0ce93a8
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-04-04 01:26:56 +0530

    BasicCodeofmain

--- EntryQueueManager_main.cpp
+++ EntryQueueManager_main.cpp
@@ -1,1 +1,23 @@
+int main()
+{
+   int capacity, gates;
+   cout << "Enter the capacity of your stadium: ";
+   cin >> capacity;
+   cout << "Enter the no of gates to be used for entering the people in the stadium: ";
+   cin >> gates;
+   EntryQueueManager e(capacity, gates);
+   e.RandomAssignment();
+   e.display();
+   e.shift();
+   cout<<"-----------------------------------"<<endl;
+   cout<<"The members in each queue after shift are "<<endl;
+   e.display();
+   cout<<"---------------------------------";
+   cout<<"The members in each queue after popping"<<endl;
+   e.pop();
+   e.display();
+   cout<<"------------------------"<<endl;
+   e.InsertNew();
+   e.display();
+}
 
