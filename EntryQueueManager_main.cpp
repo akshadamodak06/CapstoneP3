0ff8dad00fad6459abdd00f192501cb9fe2db300
Jevik Rakholiya <140234014+Jevik-R@users.noreply.github.com>
2024-04-05 01:33:58 +0530

    Modified int main with new code

--- EntryQueueManager_main.cpp
+++ EntryQueueManager_main.cpp
@@ -1,14 +1,20 @@
 int main()
 {
-   int capacity, gates;
+   int capacity, gates,poptime;
    cout << "Enter the capacity of your stadium: ";
    cin >> capacity;
    cout << "Enter the no of gates to be used for entering the people in the stadium: ";
    cin >> gates;
+   cout<<"Enter the time required for the pop in mins : ";
+   cin>>poptime;
+   int max_time = 60/poptime;
    EntryQueueManager e(capacity, gates);
    e.RandomAssignment();
    e.display();
-   e.shift();
+   int remaining=capacity/2;
+   int timef;
+   int a=0;
+
    cout<<"-----------------------------------"<<endl;
    cout<<"The members in each queue after shift are "<<endl;
    e.display();
